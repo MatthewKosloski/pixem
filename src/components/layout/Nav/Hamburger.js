c4d27e6c8cd0bed5fee9e2c40f208d6e8de64ef6
MatthewKosloski <matthewkosloski@me.com>
2018-05-29 23:23:24 -0500

    added StyledHamburger component

--- src/components/layout/Nav/Hamburger.js
+++ src/components/layout/Nav/Hamburger.js
@@ -2,42 +2,7 @@ import React, { Component }  from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import theme from '../../../theme';
-
-const { colors } = theme;
-
-const Container = styled('label')`
-	div {
-		position: relative;
-		width: 32px;
-		height: 25px;
-		cursor: pointer;
-		z-index: 1;
-	}
-	div span,
-	div span::before,
-	div span::after {
-		background-color: ${props => props.isMobileMenuVisible 
-			? colors.cadetBlue 
-			: colors.white};
-		transition: 0.15s ease-in-out;
-		width: 100%;
-		height: 4px;
-		display: block;
-		position: absolute;
-	}
-	div span {
-		top: 10px;
-	}
-	div span::before {
-		content: '';
-		top: -10px;
-	}
-	div span::after {
-		content: '';
-		bottom: -10px;
-	}
-`;
+import StyledHamburger from './StyledHamburger';
 
 const State = styled.input.attrs({
 	type: 'checkbox'
@@ -79,7 +44,7 @@ class Hamburger extends Component {
 		const { ariaControls, isMobileMenuVisible } = this.props;
 		const { ariaExpanded } = this.state;
 		return (
-			<Container
+			<StyledHamburger
 				role="button"
 				aria-controls={ariaControls}
 				aria-expanded={ariaExpanded}
@@ -88,7 +53,7 @@ class Hamburger extends Component {
 	 			<div aria-hidden="true">
 	 				<span></span>
 	 			</div>
-			</Container>
+			</StyledHamburger>
 		);
 	}
 
---
+++ src/components/layout/Nav/StyledHamburger.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import theme from '../../../theme';
+
+const { colors } = theme;
+
+export default styled('label')`
+	div {
+		position: relative;
+		width: 32px;
+		height: 25px;
+		cursor: pointer;
+		z-index: 1;
+	}
+	div span,
+	div span::before,
+	div span::after {
+		background-color: ${props => props.isMobileMenuVisible 
+			? colors.cadetBlue 
+			: colors.white};
+		transition: 0.15s ease-in-out;
+		width: 100%;
+		height: 4px;
+		display: block;
+		position: absolute;
+	}
+	div span {
+		top: 10px;
+	}
+	div span::before {
+		content: '';
+		top: -10px;
+	}
+	div span::after {
+		content: '';
+		bottom: -10px;
+	}
+`;
