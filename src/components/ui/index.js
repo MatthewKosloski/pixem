21963d5b237956d486312c484a2ac569306b4980
MatthewKosloski <matthewkosloski@me.com>
2018-06-07 01:35:01 -0500

    added input imports to ui component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,13 +1,19 @@
 import { default as Link } from './Link';
-import { default as InputCheckbox } from './InputCheckbox';
-import { default as InputRadio } from './InputRadio';
-import { default as InputText } from './InputText';
+import { default as InputCheckbox } from './InputCheckbox/index';
+import { default as InputRadio } from './InputRadio/index';
+import { default as InputText } from './InputText/index';
 import { default as Label } from './Label';
+import { default as StyledButton } from './StyledButton';
+import { default as StyledLabel } from './StyledLabel';
+import { default as StyledShakespeareButton } from './StyledShakespeareButton';
 
 export {
 	Link,
 	InputCheckbox,
 	InputRadio,
 	InputText,
-	Label
+	Label,
+	StyledButton,
+	StyledLabel,
+	StyledShakespeareButton
 };
