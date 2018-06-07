704f8e7e6f66fb9e9b86b85014f6aefd42e32d3e
MatthewKosloski <matthewkosloski@me.com>
2018-06-07 01:35:53 -0500

    added em import to util-wrapper index

--- src/util-wrappers/index.js
+++ src/util-wrappers/index.js
@@ -4,11 +4,13 @@
 *	are tightly coupled with theme.js.
 */
 
+import { default as _em } from './_em';
 import { default as _modularscalerem } from './_modularscalerem';
 import { default as _rem } from './_rem';
 import { default as _vrrem } from './_vrrem';
 
 export { 
+	_em,
 	_modularscalerem,
 	_rem,
 	_vrrem
