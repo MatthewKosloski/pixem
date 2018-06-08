541b8d2574fd4b366042fb612cb886d563dad1a8
MatthewKosloski <matthewkosloski@me.com>
2018-06-08 16:47:52 -0500

    get colors from props

--- src/components/layout/Header/Nav/Menu/StyledMobileMenu.js
+++ src/components/layout/Header/Nav/Menu/StyledMobileMenu.js
@@ -1,31 +1,43 @@
+import { css } from 'styled-components';
+
 import { _vrrem, _rem } from '../../../../../util-wrappers';
-import theme from '../../../../../theme';
 
 import StyledMenu from './StyledMenu';
 
-const { white, shakespeare } = theme.colors;
-
-export default StyledMenu.extend`
-	transform: translateY(${props => props.isMobileMenuVisible 
-		? '0' 
-		: '-100%'});
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100vh;
-	text-align: center;
-	transition: transform 0.15s ease-in-out;
-	background-color: ${white};
-	padding: ${_vrrem(4)} 0;
-	ul {
-		font-size: ${_rem(18)};
-		li {
-			a {
-				display: block;
-				color: ${shakespeare};
-				padding: ${_vrrem(0.5)} 0;
+const StyledMobileMenu = StyledMenu.extend`${
+	({isMobileMenuVisible, theme: {colors: {white, shakespeare}}}) => css`
+		transform: translateY(${isMobileMenuVisible ? '0' : '-100%'});
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100vh;
+		text-align: center;
+		transition: transform 0.15s ease-in-out;
+		background-color: ${white};
+		padding: ${_vrrem(4)} 0;
+		z-index: 1;
+		ul {
+			font-size: ${_rem(18)};
+			li {
+				a {
+					display: block;
+					color: ${shakespeare};
+					padding: ${_vrrem(0.5)} 0;
+				}
 			}
 		}
+	`
+}`;
+
+StyledMobileMenu.defaultProps = {
+	isMobileMenuVisible: true,
+	theme: {
+		colors: {
+			white: 'white',
+			shakespeare: 'shakespeare'
+		}
 	}
-`;
+};
+
+export default StyledMobileMenu;
