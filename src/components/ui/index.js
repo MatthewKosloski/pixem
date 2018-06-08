cf6d3d60f6139f507866d8482b2d90fca2fd30a0
MatthewKosloski <matthewkosloski@me.com>
2018-06-08 16:51:58 -0500

    added imports for components

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,9 +2,8 @@ import { default as Link } from './Link';
 import { default as InputCheckbox } from './InputCheckbox/index';
 import { default as InputRadio } from './InputRadio/index';
 import { default as InputText } from './InputText/index';
-import { default as Label } from './Label';
+import { default as Label } from './Label/index';
 import { default as StyledButton } from './StyledButton';
-import { default as StyledLabel } from './StyledLabel';
 import { default as StyledShakespeareButton } from './StyledShakespeareButton';
 
 export {
@@ -14,6 +13,5 @@ export {
 	InputText,
 	Label,
 	StyledButton,
-	StyledLabel,
 	StyledShakespeareButton
 };
