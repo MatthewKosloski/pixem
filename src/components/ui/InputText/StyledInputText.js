5aa15a04535863ef5c6cbad05629c7ef719fd6f3
MatthewKosloski <matthewkosloski@me.com>
2018-06-08 00:29:51 -0500

    edited padding and focus color of text input

--- src/components/ui/InputText/StyledInputText.js
+++ src/components/ui/InputText/StyledInputText.js
@@ -5,16 +5,16 @@ import { _vrrem, _rem } from '../../../util-wrappers';
 export default styled.input.attrs({
     type: 'text'
 })`${
-    ({theme: {colors: {cadetBlue, shark, shakespeare}}}) => css`
+    ({theme: {colors: {cadetBlue, shark}}}) => css`
         border-radius: 3px;
         border: 1px solid ${cadetBlue};
-        padding: ${_vrrem(0.5)} ${_vrrem(1)};
+        padding: ${_vrrem(0.5)};
         font-size: ${_rem(16)};
         color: ${shark};
         width: 100%;
 
         &:focus {
-            border-color: ${shakespeare};
+            border-color: ${shark};
         }
     `
 }`;
