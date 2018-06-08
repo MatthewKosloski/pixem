558f77a7bbeaaf59b8acb2110c1eb2bd7cac0f13
MatthewKosloski <matthewkosloski@me.com>
2018-06-08 00:22:29 -0500

    add border focus to shakespeare button

--- src/components/ui/StyledShakespeareButton.js
+++ src/components/ui/StyledShakespeareButton.js
@@ -3,11 +3,14 @@ import { css } from 'styled-components';
 import StyledButton from './StyledButton';
 
 export default StyledButton.extend`${
-	({theme: {colors: {shakespeare, white}}}) => css`
+	({theme: {colors: {shakespeare, white, shark}}}) => css`
 		border: 1px solid ${shakespeare};
 		color: ${shakespeare};
 		&:hover {
 			background-color: ${shakespeare};
 			color: ${white};
+		}
+		&:focus {
+			border-color: ${shark};
 		}
 `}`;
