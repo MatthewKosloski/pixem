5ee5a8e0e8bad90e0b7de840b4e790453847d3c8
MatthewKosloski <matthewkosloski@me.com>
2018-06-08 16:45:09 -0500

    get colors from props and remove react import

--- src/components/layout/Header/StyledHeader.js
+++ src/components/layout/Header/StyledHeader.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { _vrrem } from '../../../util-wrappers';
-import theme from '../../../theme';
 
-const { white } = theme.colors;
-
-export default styled('header')`
-	color: ${white};
-	margin-bottom: ${_vrrem(4)};
-`;
+export default styled('header')`${
+	({theme: {colors: white}}) => css`
+		color: ${white};
+		margin-bottom: ${_vrrem(4)};
+	`
+}`;
