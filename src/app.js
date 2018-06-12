abc0584722f99b730a8f026ebb4efb36d781c747
MatthewKosloski <matthewkosloski@me.com>
2018-06-12 03:56:47 -0500

    implemented modal

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ import { Hero, Stylesheet } from './components/sections';
 
 import './img/curve.svg';
 
+import { _vrrem, _rem } from './util-wrappers';
+
 injectGlobal`
 
 	*,
@@ -27,12 +29,75 @@ injectGlobal`
 		text-decoration: none;
 	}
 
+	.ReactModal__Body--open,
+	.ReactModal__Html--open {
+		overflow: hidden;
+	}
+
+	.modal__overlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: white;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.modal__content {
+		max-width: 100%;
+		width: 768px;
+		height: 475px;
+		padding: ${_vrrem(1)};
+		opacity: 0;
+		textarea {
+			box-shadow: 0 4px 32px 6px rgba(0, 0, 0, 0.33);
+			background-color: ${theme.colors.shark};
+			width: 100%;
+			height: 100%;
+			border: none;
+			border-radius: 6px;
+			font-size: ${_rem(20)};
+			color: #fff;
+			padding: ${_vrrem(1)};
+			resize: none;
+		}
+	}
+
+	.modal__content--after-open {
+		opacity: 1;
+		transition: opacity 300ms;
+	}
+
+	.modal__content--before-close {
+		opacity: 0;
+	}
+
 `;
+
+const props = {
+	reactModal: {
+		closeTimeoutMS: 150,
+		portalClassName: 'modal',
+		className: {
+			base: 'modal__content',
+			afterOpen: 'modal__content--after-open',
+			beforeClose: 'modal__content--before-close'
+		},
+		overlayClassName: {
+			base: 'modal__overlay',
+			afterOpen: 'modal__overlay--after-open',
+			beforeClose: 'modal__overlay--before-close'
+		}
+	}
+};
 
 const app = (
 	<Provider theme={theme}>
 		<Hero />
-		<Stylesheet />
+		<Stylesheet {...props} />
 	</Provider>
 );
 
--- src/components/sections/Stylesheet/index.js
+++ src/components/sections/Stylesheet/index.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Flex } from 'rebass';
+import PropTypes from 'prop-types';
+import ReactModal from 'react-modal';
 
 import { dynamicPrecision, modifyQuantityNodes } from '../../../utils';
 import { Container, Row, Column } from '../../grid';
@@ -15,6 +17,8 @@ import StyledStylesheet from './StyledStylesheet';
 import StyledTextarea from './StyledTextarea';
 
 const StyledSubmit = StyledShakespeareButton.withComponent('input');
+
+ReactModal.setAppElement(document.getElementById('app'));
 
 const testTextareaContents = `
 .title {
@@ -35,15 +39,27 @@ class Stylesheet extends Component {
 
 		this.state = {
 			textareaContents: testTextareaContents,
+			convertedStylesheet: '',
 			base: '16',
 			unit: this.units[0].value,
-			shouldPreserveOriginalValues: true
+			shouldPreserveOriginalValues: true,
+			isModalOpen: false
 		};
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
 		this.getCurrentUnitName = this.getCurrentUnitName.bind(this);
 
+	}
+
+	openModal() {
+		this.setState({isModalOpen: true});
+	}
+
+	closeModal() {
+		this.setState({isModalOpen: false});
 	}
 
 	getCurrentUnitName() {
@@ -64,7 +80,7 @@ class Stylesheet extends Component {
 	handleSubmit(e) {
 		e.preventDefault();
 
-		const newTextareaContents = modifyQuantityNodes(
+		const convertedStylesheet = modifyQuantityNodes(
 			this.state.textareaContents, 
 			(node, quantity) => {
 
@@ -81,10 +97,10 @@ class Stylesheet extends Component {
 							: ``
 					}`;
 				}
-
 		});
 
-		this.setState({textareaContents: newTextareaContents});
+		this.setState({ convertedStylesheet });
+		this.openModal();
 
 	}
 
@@ -94,98 +110,133 @@ class Stylesheet extends Component {
 			base, 
 			unit, 
 			shouldPreserveOriginalValues,
-			textareaContents
+			textareaContents,
+			isModalOpen,
+			convertedStylesheet
 		} = this.state;
 
 		return (
-			<StyledStylesheet>
-				<Container>
-					<Row mb={3}>
-						<Column width={1}>
-							<StyledTextarea 
-								placeholder="// paste your CSS style sheet here"
-								name="textareaContents"
-								value={textareaContents}
-								onChange={this.handleChange} />
-						</Column>
-					</Row>
-				</Container>
-				<Container small="true">
-					<Row>
-						<Column width={1}>
-							<form onSubmit={this.handleSubmit}>
-								<Row alignItems="center" mb={2}>
-									<Column width={[1, 7/12]} mb={[1, 0]}>
-										<Label
-											htmlFor="base"
-											title="Base Pixel Size"
-											tooltipText="This is tooltip text" />	
-									</Column>
-									<Column width={[1, 5/12]}>
-										<InputText 
-											id="base"
-											name="base"
-											value={base}
-											onChange={this.handleChange} />
-									</Column>
-								</Row>
-								<Row alignItems="center" mb={2}>
-									<Column width={[1, 7/12]} mb={[1, 0]}>
-										<Label
-											htmlFor="shouldPreserveOriginalValues"
-											title="Preserve Original Values"
-											tooltipText="This is tooltip text" />	
-									</Column>
-									<Column width={[1, 5/12]}>
-										<InputCheckbox
-											id="shouldPreserveOriginalValues"
-											name="shouldPreserveOriginalValues"
-											checked={shouldPreserveOriginalValues}
-											onChange={this.handleChange} />
-									</Column>
-								</Row>
-								<Row alignItems="center" mb={2}>
-									<Column width={[1, 7/12]} mb={[1, 0]}>
-										<Label
-											title="Conversion Unit"
-											tooltipText="This is tooltip text"/>	
-									</Column>
-									<Column width={[1, 5/12]}>
-										<Flex>
-											<div style={{flex: '1'}}>
-												<InputRadio
-													title="EMs"
-													name="unit"
-													value={this.units[0].value}
-													checked={unit === this.units[0].value}
-													onChange={this.handleChange} />
-											</div>
-									
-											<div style={{flex: '1'}}>
-												<InputRadio
-													title="REMs"
-													name="unit"
-													value={this.units[1].value}
-													checked={unit === this.units[1].value}
-													onChange={this.handleChange} />
-											</div>
-										</Flex>
-									</Column>
-								</Row>
-								<Row justifyContent="center">
-									<Column>
-										<StyledSubmit 
-											type="submit" 
-											value="Convert" />
-									</Column>
-								</Row>
-							</form>
-						</Column>
-					</Row>
-				</Container>
-			</StyledStylesheet>
+			<Fragment>
+				<ReactModal
+					isOpen={isModalOpen}
+					onRequestClose={this.closeModal}
+					{...this.props.reactModal}>
+					<textarea 
+						value={convertedStylesheet}
+						readOnly />
+				</ReactModal>
+				<StyledStylesheet>
+					<Container>
+						<Row mb={3}>
+							<Column width={1}>
+								<StyledTextarea 
+									placeholder="// paste your CSS style sheet here"
+									name="textareaContents"
+									value={textareaContents}
+									onChange={this.handleChange} />
+							</Column>
+						</Row>
+					</Container>
+					<Container small="true">
+						<Row>
+							<Column width={1}>
+								<form onSubmit={this.handleSubmit}>
+									<Row alignItems="center" mb={2}>
+										<Column width={[1, 7/12]} mb={[1, 0]}>
+											<Label
+												htmlFor="base"
+												title="Base Pixel Size"
+												tooltipText="This is tooltip text" />	
+										</Column>
+										<Column width={[1, 5/12]}>
+											<InputText 
+												id="base"
+												name="base"
+												value={base}
+												onChange={this.handleChange} />
+										</Column>
+									</Row>
+									<Row alignItems="center" mb={2}>
+										<Column width={[1, 7/12]} mb={[1, 0]}>
+											<Label
+												htmlFor="shouldPreserveOriginalValues"
+												title="Preserve Original Values"
+												tooltipText="This is tooltip text" />	
+										</Column>
+										<Column width={[1, 5/12]}>
+											<InputCheckbox
+												id="shouldPreserveOriginalValues"
+												name="shouldPreserveOriginalValues"
+												checked={shouldPreserveOriginalValues}
+												onChange={this.handleChange} />
+										</Column>
+									</Row>
+									<Row alignItems="center" mb={2}>
+										<Column width={[1, 7/12]} mb={[1, 0]}>
+											<Label
+												title="Conversion Unit"
+												tooltipText="This is tooltip text"/>	
+										</Column>
+										<Column width={[1, 5/12]}>
+											<Flex>
+												<div style={{flex: '1'}}>
+													<InputRadio
+														title="EMs"
+														name="unit"
+														value={this.units[0].value}
+														checked={unit === this.units[0].value}
+														onChange={this.handleChange} />
+												</div>
+										
+												<div style={{flex: '1'}}>
+													<InputRadio
+														title="REMs"
+														name="unit"
+														value={this.units[1].value}
+														checked={unit === this.units[1].value}
+														onChange={this.handleChange} />
+												</div>
+											</Flex>
+										</Column>
+									</Row>
+									<Row justifyContent="center">
+										<Column>
+											<StyledSubmit 
+												type="submit" 
+												value="Convert" />
+										</Column>
+									</Row>
+								</form>
+							</Column>
+						</Row>
+					</Container>
+				</StyledStylesheet>
+			</Fragment>
 		);
 	}
 }
+
+Stylesheet.propTypes = {
+	reactModal: PropTypes.shape({
+		closeTimeoutMS: PropTypes.number,
+		portalClassName: PropTypes.string,
+		className: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.shape({
+				base: PropTypes.string,
+				afterOpen: PropTypes.string,
+				beforeClose: PropTypes.string
+			})
+		]),
+		overlayClassName: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.shape({
+				base: PropTypes.string,
+				afterOpen: PropTypes.string,
+				beforeClose: PropTypes.string
+			})
+		])
+	})
+};
 
 export default Stylesheet;
