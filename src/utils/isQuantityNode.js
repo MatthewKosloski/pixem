91ed4ee4ec86559bbd518bb7828d0574077d02a9
MatthewKosloski <matthewkosloski@me.com>
2018-06-13 04:17:05 -0500

    isQuantityNode util

---
+++ src/utils/isQuantityNode.js
@@ -0,0 +1,14 @@
+/**
+  * Returns a boolean indicating if the
+  * provided node is a word, has a value
+  * property, and has a quantity.
+  * 
+  * @param node {Object}
+  * @param quantity {Object} An object with
+  * number and unit keys. See docs here: 
+  * https://goo.gl/AWzZoC
+  */
+ export default (node, quantity) =>
+    node.type === 'word' && 
+    node.hasOwnProperty('value') && 
+    quantity;
