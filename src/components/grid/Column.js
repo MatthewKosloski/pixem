bd06a19102bd64c0c110f241dd4f97cb4ef4dea9
MatthewKosloski <matthewkosloski@me.com>
2018-07-04 03:14:09 -0500

    added default padding prop to Column

--- src/components/grid/Column.js
+++ src/components/grid/Column.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Box } from 'rebass';
 
 const Column = props => (
-	<Box {...props} px={1}>
+	<Box {...props}>
 		{props.children}
 	</Box>
 );
+
+Column.defaultProps = {
+	px: 1
+};
 
 export default Column;
