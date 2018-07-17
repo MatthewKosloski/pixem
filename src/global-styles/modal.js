2169bd13e2d22df1b6e49b338b776f4355f2f356
MatthewKosloski <matthewkosloski@me.com>
2018-07-17 04:37:54 -0500

    update modal styles

--- src/global-styles/modal.js
+++ src/global-styles/modal.js
@@ -1,7 +1,7 @@
 import theme from '../theme';
 import { _vrrem, _rem, _modularscalerem } from '../util-wrappers';
 
-const { cadetBlue, shark } = theme.colors;
+const { cadetBlue, white } = theme.colors;
 const { sm, md } = theme.media;
 
 export default `
@@ -27,75 +27,31 @@ export default `
     }
 
     .modal__content {
-        max-width: 100%;
+        background-color: ${white};
+        border-radius: 6px;
+        box-shadow: 0 8px 32px 16px rgba(0, 0, 0, 0.2);
+        max-width: calc(100% - ${_vrrem(2)});
         width: 768px;
-        margin: 0 auto;
-        padding: ${_vrrem(5)} ${_vrrem(1)} ${_vrrem(1)} ${_vrrem(1)};
+        margin-left: auto;
+        margin-right: auto;
+        padding: ${_vrrem(1)};
         opacity: 0;
-        p {
-            text-align: center;
-            font-size: ${_rem(18)};
-            @media ${sm} {
-                font-size: ${_modularscalerem(1)};
-            }
-            @media ${md} {
-                font-size: ${_modularscalerem(2)};
-            }
-            font-family: 'Open Sans', sans-serif;
-            margin-bottom: ${_vrrem(3)};
-            color: ${cadetBlue};
-        }
-        textarea {
-            box-shadow: 0 4px 32px 6px rgba(0, 0, 0, 0.33);
-            background-color: ${shark};
-            width: 100%;
-            min-height: 475px;
-            border: none;
-            border-radius: 6px;
-            font-size: ${_rem(12)};
-            @media ${sm} {
-                font-size: ${_rem(16)};
-            }
-            @media ${md} {
-                font-size: ${_rem(20)};
-            }
-            color: #fff;
-            padding: ${_vrrem(1)};
-            resize: none;
-        }
-        span {
+        position: relative;
+        margin-top: ${_vrrem(1)};
+        margin-bottom: ${_vrrem(1)};
+        transform: scale3d(0, 0, 0);
+        @media (min-height: 500px) {
             position: absolute;
-            cursor: pointer;
-            right: ${_vrrem(1)};
-            top: ${_vrrem(1)};
-            width: 24px;
-            height: 24px;
-            @media ${sm} {
-                width: 32px;
-                height: 32px;
-            }
-            &::before,
-            &::after {
-                content: '';
-                width: 100%;
-                height: 4px;
-                background-color: ${cadetBlue};
-                position: absolute;
-                top: 50%;
-                left: 0;
-            }
-            &::before {
-                transform: rotate(45deg);
-            }
-            &::after {
-                transform: rotate(-45deg);
-            }
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) scale3d(0.5, 0.5, 0.5);
         }
     }
 
     .modal__content--after-open {
         opacity: 1;
-        transition: opacity 300ms;
+        transition: transform 150ms ease, opacity 300ms;
+        transform: translate(-50%, -50%) scale3d(1, 1, 1);
         &:focus {
             outline: 0;
         }
@@ -103,6 +59,32 @@ export default `
 
     .modal__content--before-close {
         opacity: 0;
+    }
+
+    .modal__close {
+        position: absolute;
+        cursor: pointer;
+        right: ${_vrrem(1)};
+        top: ${_vrrem(1)};
+        width: 20px;
+        height: 20px;
+        &::before,
+        &::after {
+            content: '';
+            width: 100%;
+            height: 4px;
+            background-color: ${cadetBlue};
+            border-radius: 100px;
+            position: absolute;
+            top: 50%;
+            left: 0;
+        }
+        &::before {
+            transform: rotate(45deg);
+        }
+        &::after {
+            transform: rotate(-45deg);
+        }
     }
 
 `;
