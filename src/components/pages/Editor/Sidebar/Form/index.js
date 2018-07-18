dee5eeebb42676d82f15893f2e38f1b4407fcaee
MatthewKosloski <matthewkosloski@me.com>
2018-07-18 02:27:32 -0500

    added radios and checkbox to sidebar

--- src/components/pages/Editor/Sidebar/Form/index.js
+++ src/components/pages/Editor/Sidebar/Form/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 
 import { _vrrem } from '../../../../../util-wrappers';
+import { Row, Column } from '../../../../atoms';
 import {
     TooltipLabel,
     InputText,
-    InputRadio
+    InputRadio,
+    InputCheckbox
 } from '../../../../molecules';
 
-const StyledForm = styled('div')`
-    input {
-        margin-top: ${_vrrem(1)};
-    }
+
+const StyledFormItem = styled('div')`
+    margin-bottom: ${_vrrem(1.5)};
 `;
 
 const Form = ({
@@ -21,17 +22,53 @@ const Form = ({
     handleChange    
 }) => {
     return (
-        <StyledForm>
-            <TooltipLabel
-                htmlFor="base"
-                title="Base Pixel Size"
-                tooltipText="This is tooltip text" />
-            <InputText
-                id="base"
-                name="base"
-                value={base}
-                onChange={handleChange} />	
-        </StyledForm>
+        <Fragment>
+            <StyledFormItem>
+                <TooltipLabel
+                    htmlFor="base"
+                    title="Base Pixel Size"
+                    tooltipText="This is tooltip text" />
+                <InputText
+                    id="base"
+                    name="base"
+                    value={base}
+                    onChange={handleChange} />	
+            </StyledFormItem>
+            <StyledFormItem>
+                <TooltipLabel
+                    title="Conversion Unit"
+                    tooltipText="This is tooltip text" />
+                <Row>
+                    <Column>
+                        <InputRadio
+                            title="EMs"
+                            name="unit"
+                            value={'em'}
+                            checked={unit === 'em'}
+                            onChange={handleChange} />
+                    </Column>
+                    <Column>
+                        <InputRadio
+                            title="REMs"
+                            name="unit"
+                            value={'rem'}
+                            checked={unit === 'rem'}
+                            onChange={handleChange} />
+                    </Column>
+                </Row>
+            </StyledFormItem>
+            <StyledFormItem>
+                <TooltipLabel
+                    htmlFor="shouldPreserveOriginalValues"
+                    title="Preserve Original Values"
+                    tooltipText="This is tooltip text" />
+                <InputCheckbox
+                    id="shouldPreserveOriginalValues"
+                    name="shouldPreserveOriginalValues"
+                    checked={shouldPreserveOriginalValues}
+                    onChange={handleChange} />
+            </StyledFormItem>
+        </Fragment>
     );
 };
 
