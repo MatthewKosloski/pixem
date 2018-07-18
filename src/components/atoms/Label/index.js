6f7c9918fb868dc517197aa9ec5fb4baca9dc80f
MatthewKosloski <matthewkosloski@me.com>
2018-07-18 02:24:03 -0500

    added bottom margin to label atom

--- src/components/atoms/Label/index.js
+++ src/components/atoms/Label/index.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-import { _rem } from '../../../util-wrappers';
+import { _rem, _vrrem } from '../../../util-wrappers';
 
 export default styled('label')`${
     ({theme: {colors: {cadetBlue}}}) => css`
@@ -8,5 +8,7 @@ export default styled('label')`${
         font-size: ${_rem(14)};
         text-transform: uppercase;
         color: ${cadetBlue};
+        margin-bottom: ${_vrrem(1)};
+        display: block;
     `
 }`;
