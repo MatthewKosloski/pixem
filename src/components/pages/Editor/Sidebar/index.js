3a6c3a1e2599e0ea601d50b0788b76ddd56a2143
MatthewKosloski <matthewkosloski@me.com>
2018-07-27 00:14:45 -0500

    made Sidebar a stateful component to manage viewport width on resize

--- src/components/pages/Editor/Sidebar/index.js
+++ src/components/pages/Editor/Sidebar/index.js
@@ -1,35 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 
 import { _vrrem, _rem } from '../../../../util-wrappers';
 
-import Headings from './Headings';
+import Header from './Header';
 import Form from './Form';
 
 const StyledSidebar = styled('aside')`${
-    ({theme: {media: {md}, colors: {shark, white, mako}}}) => css`
+    ({theme: {
+        media: {md}, 
+        colors: {shark, white, cadetBlue, mako}
+    }}) => css`
         background-color: ${shark};
         border-bottom: 2px solid ${mako};
-        height: 100vh;
-        color: ${white};
-        font-size: ${_rem(16)};
+        color: ${cadetBlue};
+        font-size: ${_rem(14)};
         padding: ${_vrrem(1)};
         a {
             color: ${white};
         }
         @media ${md} {
+            height: 100vh;
             border-bottom: none;
             border-right: 2px solid ${mako};
+            font-size: ${_rem(16)};
         }
 `}`;
 
-const Sidebar = props => {
-    return (
-        <StyledSidebar>
-            <Headings />
-            <Form {...props}/>
-        </StyledSidebar>
-    );
-};
+class Sidebar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewportWidth: window.innerWidth,
+            isHamburgerOpen: false
+        };
+        this.onResize = this.onResize.bind(this);
+        this.isMobile = this.isMobile.bind(this);
+        this.handleHamburgerClick = this.handleHamburgerClick.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.onResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onResize);
+    }
+
+    onResize() {
+        const viewportWidth = window.innerWidth;
+        this.setState({viewportWidth});
+    }
+
+    isMobile() {
+        return this.state.viewportWidth <= 768;
+    }
+
+    handleHamburgerClick() {
+        this.setState({
+            isHamburgerOpen: !this.state.isHamburgerOpen
+        });
+    }
+
+    render() {
+        return (
+            <StyledSidebar>
+                <Header 
+                    onHamburgerClick={this.handleHamburgerClick}
+                    isMobile={this.isMobile()}/>
+                <Form 
+                    isMobile={this.isMobile()}
+                    isMobileFormVisible={this.state.isHamburgerOpen} 
+                    {...this.props}/>
+            </StyledSidebar>
+        );   
+    }
+
+}
 
 export default Sidebar;
