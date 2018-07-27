2d81e5ba20751cb1cd49ba2898ff8c589d40898d
MatthewKosloski <matthewkosloski@me.com>
2018-07-27 00:15:37 -0500

    rewrote conversion function to properly display css comments

--- src/components/pages/Editor/index.js
+++ src/components/pages/Editor/index.js
@@ -1,10 +1,9 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import valueParser from 'postcss-value-parser';
 
 import * as utils from '../../../utils';
 
-import { TwoColumn } from '../../templates';
-import { Modal } from '../../molecules';
+import { TwoColumnTemplate } from '../../templates';
 
 import Sidebar from './Sidebar';
 import PanelManager from './PanelManager';
@@ -14,15 +13,28 @@ const defaultUserInput = `.title {
 	font-size: 16px;
 	line-height: 1.3;
 	width: 80%; 
-  }`;  
+  }
 
-const Settings = () => (
-	<p>Settings</p>
-);
+hr {
+    border-color: #f05f40;
+    border-width: 3px;
+    max-width: 50px;
+}
 
-const Help = () => (
-	<p>Help</p>
-);
+p {
+    font-size: 16px;
+    line-height: 1.5;
+    margin-bottom: 20px;
+}
+
+section {
+    padding: 100px 0;
+}
+
+aside {
+    padding: 50px 15px;
+}
+`;  
 
 class Editor extends Component {
 
@@ -34,23 +46,82 @@ class Editor extends Component {
 			result: '',
 			base: '16',
 			unit: 'em',
-			shouldPreserveOriginalValues: false,
-			modal: {
-				isOpen: false,
-				activeChild: 'help'
-			}
+			shouldPreserveOriginalValues: false
 		};
 
 		this.handleUserInputChange = this.handleUserInputChange.bind(this);
-		this.renderModalChildren = this.renderModalChildren.bind(this);
+		this.convertPixelNodes = this.convertPixelNodes.bind(this);
 		this.getResult = this.getResult.bind(this);
-		this.openModal = this.openModal.bind(this);
-		this.closeModal = this.closeModal.bind(this);
 		this.handleChange = this.handleChange.bind(this);
-		this.renderColumnOne = this.renderColumnOne.bind(this);
-		this.renderColumnTwo = this.renderColumnTwo.bind(this);
+		this.renderFirstColumn = this.renderFirstColumn.bind(this);
+		this.renderSecondColumn = this.renderSecondColumn.bind(this);
 	}
 
+	/**
+	*	Predicated on a provided base size, it
+	*	converts a pixel string to an em or rem string.
+	*
+	*	@example
+	* 	// returns '1rem'
+	* 	emRem(16, '16px', 'em', false)
+	*
+	*	@param {number} base
+	*	@param {string} px
+	*   @param {boolean} [isEm = true]
+	*	@return {string}
+	*/
+	emRem(base, px, isEm = true) {
+		const pxRegex = /(\d+)(px;?)/;
+		
+		if(!pxRegex.test(px)) return;
+		
+		const result = pxRegex.exec(px);
+
+		const number = utils.dynamicPrecision(result[1] / base);
+		const unit = result[2].replace(/px/, isEm ? 'em' : 'rem');
+		
+		return `${number}${unit}`;
+	}
+
+	/**
+	*	Predicated on a provided base size, it
+	*	converts a pixel string to an em string.
+	*
+	*	@example
+	* 	// returns '1em'
+	* 	em(16, '16px')
+	*
+	*	@param {number} base
+	*	@param {string} px
+	*	@return {string}
+	*/
+	em(base, px) {
+		return this.emRem(base, px);
+	}
+
+	/**
+	*	Predicated on a provided base size, it
+	*	converts a pixel string to a rem string.
+	*
+	*	@example
+	* 	// returns '1rem'
+	* 	rem(16, '16px')
+	*
+	*	@param {number} base
+	*	@param {string} px
+	*	@return {string}
+	*/
+	rem(base, px) {
+		return this.emRem(base, px, false);
+	}
+
+	/**
+	 * When the user pastes in CSS into
+	 * the editable textarea, the value is
+	 * stored in userInput.  After we have
+	 * the user's code, we get the converted
+	 * result.
+	 */
 	handleUserInputChange(editor, data, value) {
 		this.setState({
 			userInput: value
@@ -61,127 +132,156 @@ class Editor extends Component {
 		});
 	}
 
-	getResult() {
-		const { 
-			userInput, 
-			base, 
-			unit, 
-			shouldPreserveOriginalValues 
-		} = this.state;
+	/**
+	 * Returns a one-line CSS comment using
+	 * the provided string.
+	 * @param {string} str 
+	 * @return {string}
+	 */
+	makeCSSComment(str) {
+		return `/* ${str} */`;
+	}
 
-		return this.convertPixelNodes({
-			stylesheet: userInput,
-			toUnit: unit,
+	/**
+	 * Returns a boolean indicating if $node
+	 * is a CSS property (e.g., "padding:")
+	 * 
+	 * @param {object} node
+	 * @param {number} index
+	 * @param {array} nodes
+	 * @return {boolean}
+	 */
+	isPropertyNode(node, index, nodes) {
+		const nextNode = nodes[index + 1];
+		if(nextNode === undefined) return;
+		return node.type === 'word' && 
+			nextNode.type === 'div' && 
+			nextNode.value === ':';
+	}
+
+	/**
+	 * Keeps incrementing $index until it finds another
+	 * CSS property within $nodes.
+	 * @param {object} node 
+	 * @param {index} index 
+	 * @param {nodes} nodes 
+	 */
+	getNextPropertyNode(node, index, nodes) {
+		let nextPropertyNode;
+		let currIndex = index + 1;
+		while(currIndex < nodes.length) {
+			let currNode = nodes[currIndex];
+			if(this.isPropertyNode(currNode, currIndex, nodes)) {
+				nextPropertyNode = currNode;
+				break;
+			}
+			currIndex++;
+		}
+		return nextPropertyNode;
+	}
+
+	/**
+	 * Takes in a stylesheet with pixel (px)
+	 * values and returns a stylesheet with
+	 * converted v
+	 * 
+	 * @param {object}
+	 * @return {string} 
+	 */
+	convertPixelNodes({
+		userInput,
+		shouldPreserveOriginalValues,
+		unit,
+		base
+	}) {
+
+		const pxRegex = /(\d+)(px;?)/;
+        const parsedStylesheet = valueParser(userInput); 
+
+        parsedStylesheet.walk((node, index, nodes) => {
+
+            if(this.isPropertyNode(node, index, nodes)) {
+                let propertyNodeDivider = nodes[index + 1];
+                let nextPropertyNode = this.getNextPropertyNode(
+					node, index, nodes);
+
+                let propertyNodeDividerIndex = nodes.indexOf(
+					propertyNodeDivider);
+
+                let nextPropertyNodeIndex = nodes.indexOf(
+					nextPropertyNode);
+
+                let currIndex = propertyNodeDividerIndex + 1;
+
+                let endIndex = nextPropertyNodeIndex === -1 
+                    ? nodes.length 
+                    : nextPropertyNodeIndex - 1;
+
+                let values = [], indices = [];
+                while(currIndex < endIndex) {
+                    let currNode = nodes[currIndex];
+                    let quantity = valueParser.unit(currNode.value);
+                    if(currNode.type === 'word' && quantity) {
+                        values.push(currNode.value);
+                        indices.push(currIndex);
+                        if(pxRegex.test(currNode.value)) {
+                            currNode.value = utils.isEm(unit) 
+                                ? this.em(base, currNode.value) 
+                                : this.rem(base, currNode.value);
+                        }
+                    }
+                    currIndex++;
+                }
+
+                if(shouldPreserveOriginalValues) {
+                    let lastIndex = utils.getLastArrayItem(indices);
+                    if(lastIndex !== undefined) {
+                        let comment = this.makeCSSComment(
+                            utils.stripSemicolons(String(values.join(' '))));
+                        // only add comments for px values
+                        if(pxRegex.test(comment)) {
+                            nodes[lastIndex].value += ` ${comment}`;
+                        }
+                    }
+                }
+            }
+
+		});
+		
+		return valueParser.stringify(parsedStylesheet);
+	}
+
+	/**
+	 * Passes in the state to convertPixelNodes
+	 * and returns the result.
+	 * 
+	 * @return {string}
+	 */
+	getResult() {
+		const {
+			userInput,
 			shouldPreserveOriginalValues,
+			unit,
+			base
+		} = this.state;
+		return this.convertPixelNodes({
+			userInput,
+			shouldPreserveOriginalValues,
+			unit,
 			base
 		});
 	}
 
 	/**
-	 * Walks through each node within the stylesheet, 
-	 * performing a callback on each node of type "word" 
-	 * whose property has a quantity value.
-	 * 
-	 * See the definition of the "word" node type here:
-	 * https://github.com/TrySound/postcss-value-parser#word
-	 * 
-	 * @param stylesheet {String} The stylesheet/CSS to be parsed 
-	 * into an object
-	 * @param cb {Function} Callback to run on each node. The
-	 * node itself, along with the number and unit of the property 
-	 * value is passed to the callback.
-	 * @return {String} The stylesheet after the modifications
+	 * The one event handler responsible for
+	 * setting the state.
 	 */
-	modifyQuantityNodes(stylesheet, cb) {
-
-		const parsedStylesheet = valueParser(stylesheet);
-
-		parsedStylesheet.walk(node => {
-
-			const quantity = valueParser.unit(node.value);
-
-			// We only care about nodes that have quantities
-			if(utils.isQuantityNode(node, quantity)) {
-				cb(node, quantity);
-			}
-		});
-		return parsedStylesheet.toString();
-	}
-
-	/**
-	 * Performs a callback function on each
-	 * quantity node it receives from modifyQuantityNodes.
-	 * If the node has a pixel unit, we convert the quantity
-	 * to either ems or rems.
-	 * 
-	 * @param {Object}  
-	 */
-	convertPixelNodes({
-		stylesheet,
-		base,
-		toUnit,
-		shouldPreserveOriginalValues
-	}) {
-		return this.modifyQuantityNodes(
-			stylesheet,
-			(node, quantity) => {
-	
-				// quantity is an object like {number: '2', unit: 'rem'}
-				
-				if(utils.isPixel(quantity.unit)) {
-	
-					const newValue = utils.isEm(toUnit)
-						? utils.em(base, quantity.number)
-						: utils.rem(base, quantity.number);
-	
-					const originalValueComment = ` /* ${
-						utils.removeSemicolons(node.value)
-					} */`;
-	
-					node.value = `${newValue};${
-						shouldPreserveOriginalValues
-							? originalValueComment
-							: ``
-					}`;
-				}
-	
-			}
-		);
-	}
-
-	openModal() {
-		this.setState({
-			modal: {
-				...this.state.modal,
-				isOpen: true
-			}
-		});
-	}
-
-	closeModal() {
-		this.setState({
-			modal: {
-				...this.state.modal,
-				isOpen: false
-			}
-		});
-	}
-
-	renderModalChildren() {
-		const children = {
-			settings: Settings,
-			help: Help
-		};
-		const Component = children[
-			this.state.modal.activeChild
-		];
-		return <Component />;
-	}
-
 	handleChange(e) {
 		const { type, checked, value, name } = e.target;
 		this.setState({
-			[name]: type === 'checkbox' ? checked : value,
+			[name]: type === 'checkbox' 
+				? checked 
+				: value,
 		}, () => {
 			this.setState({
 				result: this.getResult()
@@ -189,7 +289,12 @@ class Editor extends Component {
 		});
 	}
 
-	renderColumnOne() {
+	/**
+	 * Renders the sidebar.
+	 * 
+	 * @return {ReactElement}
+	 */
+	renderFirstColumn() {
 		const {
 			base,
 			unit,
@@ -205,7 +310,12 @@ class Editor extends Component {
 		);
 	}
 
-	renderColumnTwo() {
+	/**
+	 * Renders the two textareas.
+	 * 
+	 * @return {ReactElement}
+	 */
+	renderSecondColumn() {
 		const { userInput, result } = this.state;
 		return (
 			<PanelManager>
@@ -225,16 +335,9 @@ class Editor extends Component {
 
 	render() {
 		return(
-			<Fragment>
-				<Modal
-					isOpen={this.state.modal.isOpen}
-					onRequestClose={this.closeModal}>
-					{this.renderModalChildren()}
-				</Modal>
-				<TwoColumn 
-					columnOneNode={this.renderColumnOne()}
-					columnTwoNode={this.renderColumnTwo()}/>
-			</Fragment>
+			<TwoColumnTemplate 
+				firstColumn={this.renderFirstColumn()}
+				secondColumn={this.renderSecondColumn()}/>
 		);
 	}
 }
