a4d607fd22a68326a6be0a43547b77d9f814900b
MatthewKosloski <matthewkosloski@me.com>
2018-08-03 15:17:33 -0500

    no horizontal padding on Hamburger parent column

--- src/components/pages/Editor/Sidebar/Header/index.tsx
+++ src/components/pages/Editor/Sidebar/Header/index.tsx
@@ -33,7 +33,7 @@ class Header extends React.PureComponent<IPropTypes> {
                         </Column>
                         <Column width={[3/12]}>
                             <Row justifyContent="flex-end">
-                                <Column>
+                                <Column px={0}>
                                     <Hamburger 
                                         ariaLabel="Toggle Settings View"
                                         ariaControls="sidebar-mobile-form"
