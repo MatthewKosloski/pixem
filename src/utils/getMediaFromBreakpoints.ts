9ef9136d735aa76f9ce5b7846bdb8c3214a6dd67
MatthewKosloski <matthewkosloski@me.com>
2018-08-03 14:49:58 -0500

    added getMediaFromBreakpoints util func

---
+++ src/utils/getMediaFromBreakpoints.ts
@@ -0,0 +1,24 @@
+interface IMedia {
+    [key: string]: string;
+}
+
+/**
+ * Returns an object of interpolated media query 
+ * strings.  $names holds the keys and the values
+ * are derived from $breakpoints.
+ * 
+ * @example
+ * getMediaFromBreakpoints(['sm'], ['500px'])
+ * // returns {sm: '(min-width: 500px)'}
+ */
+export default (names: string[], breakpoints: string[]): IMedia => {
+
+	let media: IMedia = {};
+
+	breakpoints.forEach((breakpoint, index) => {
+		const key = names[index];
+		media[key] = `(min-width: ${breakpoint})`;
+	});
+
+	return media;
+}
