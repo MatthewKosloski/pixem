c069b880c19ffd5a0b4fedc4ceeec138267d97fb
MatthewKosloski <matthewkosloski@me.com>
2018-08-03 14:50:31 -0500

    added getVrSequence util func

---
+++ src/utils/getVrSequence.ts
@@ -0,0 +1,19 @@
+import vr from './vr';
+
+/**
+ * Returns a sequence of vertical rhythm numbers from
+ * a step of 0 to $quantity.
+ */
+export default (
+	quantity: number, 
+	base: number, 
+	ratio: number
+): number[] => {
+	let spaces = [];
+
+	for(let i = 0; i < quantity; i++) {
+		spaces.push(vr(base, ratio, i));
+	}
+
+	return spaces;
+}
