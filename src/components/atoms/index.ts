079704f3605580411b09ce0af8f3751db4364da7
MatthewKosloski <matthewkosloski@me.com>
2018-08-04 02:18:04 -0500

    import headings from atoms pacakge

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -3,10 +3,10 @@ import {default as Checkbox} from './Checkbox';
 import {default as CheckboxIcon} from './CheckboxIcon';
 import {default as Column} from './Column';
 import {default as Container} from './Container';
-// import {default as H1} from './H1';
-// import {default as H2} from './H2';
-// import {default as H3} from './H3';
-// import {default as H4} from './H4';
+import {default as H1} from './H1';
+import {default as H2} from './H2';
+import {default as H3} from './H3';
+import {default as H4} from './H4';
 import {default as HelpIcon} from './HelpIcon';
 import {default as Label} from './Label';
 import {default as Link} from './Link';
@@ -20,10 +20,10 @@ export {
     CheckboxIcon,
     Column,
     Container,
-    // H1,
-    // H2,
-    // H3,
-    // H4,
+    H1,
+    H2,
+    H3,
+    H4,
     HelpIcon,
     Label,
     Link,
