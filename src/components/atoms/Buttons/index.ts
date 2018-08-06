621c7fcc9db4f10449119b38063b729ec3d5ed06
MatthewKosloski <matthewkosloski@me.com>
2018-08-06 02:45:51 -0500

    add shark button to package

--- src/components/atoms/Buttons/index.ts
+++ src/components/atoms/Buttons/index.ts
@@ -1,7 +1,9 @@
 import { default as Default } from './Default';
 import { default as Shakespeare } from './Shakespeare';
+import { default as Shark } from './Shark';
 
 export default {
     Default,
     Shakespeare,
+    Shark
 };
