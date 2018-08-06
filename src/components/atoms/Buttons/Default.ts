8f8420011b5e6662b4c4b7cd63c8b4169540e4f1
MatthewKosloski <matthewkosloski@me.com>
2018-08-06 02:45:06 -0500

    changes to default button styles

--- src/components/atoms/Buttons/Default.ts
+++ src/components/atoms/Buttons/Default.ts
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 
-import { _rem, _vrrem } from '../../../util-wrappers';
+import { _rem, _vrrem, _vrem, _em } from '../../../util-wrappers';
 
 export default styled('a')`
 	font-size: ${_rem(14)};
-	border-radius: 3px;
-	padding: 0 ${_vrrem(2)};
-	line-height: 45px;
-	height: 45px;
-	display: block;
+	border-radius: ${_em(6)};
+	padding: 0 ${_vrem(1.5)};
+	line-height: ${_vrrem(2.5)};
+	height: ${_vrrem(2.5)};
+	display: inline-block;
 	cursor: pointer;
 	background-color: transparent;
 	text-transform: uppercase;
+	text-decoration: none;
 	font-weight: 700;
 `;
