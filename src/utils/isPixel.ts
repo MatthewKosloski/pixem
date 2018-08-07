744196adf4cf5f0caa6994eb27f372e8a6b7d34a
MatthewKosloski <matthewkosloski@me.com>
2018-08-07 14:33:31 -0500

    removed unused util funcs

--- src/utils/isPixel.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns true if "px" is found
- * in a string.
- */
-export default (str: string): boolean => 
-    /px/.test(str);
--- src/utils/isQuantityNode.ts
+++
@@ -1,9 +0,0 @@
-/**
-  * Returns a boolean indicating if the
-  * provided node is a word, has a value
-  * property, and has a quantity.
-  */
- export default (node: any, quantity: any): boolean =>
-  node.type === 'word' && 
-  node.hasOwnProperty('value') && 
-  quantity;
