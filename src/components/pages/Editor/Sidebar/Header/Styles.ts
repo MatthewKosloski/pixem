9be4dcca414350ab83efb1870d9ad65f19388023
MatthewKosloski <matthewkosloski@me.com>
2018-08-07 14:34:22 -0500

    indent

--- src/components/pages/Editor/Sidebar/Header/Styles.ts
+++ src/components/pages/Editor/Sidebar/Header/Styles.ts
@@ -3,11 +3,9 @@ import styled, { css } from 'styled-components';
 import { _vrrem } from '@util-wrappers';
 
 export const Container = styled('div')`${
-    ({
-        theme: {
-            media: { md }
-        }
-    }) => css`
+    ({theme: {
+        media: { md }
+    }}) => css`
         @media ${md} {
             margin-bottom: ${_vrrem(3)};
         }
