a7f01095c3947ab9227f9a4ab200589d42827054
MatthewKosloski <matthewkosloski@me.com>
2018-08-11 02:49:40 -0500

    added head to editor page

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -4,12 +4,19 @@ import { Provider } from 'rebass';
 
 import theme from '@src/theme';
 import { Editor } from '@pages';
+import { editor } from '@data';
+
+const { url, title, description } = editor.meta;
 
 export default () => {
     return (
         <React.Fragment>
             <Helmet>
-                <title>Pixem</title>
+                <meta property="og:url" content={url} />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta name="description" content={description} />
+                <title>{title}</title>
             </Helmet>
             <Provider theme={theme}>
                 <Editor />
