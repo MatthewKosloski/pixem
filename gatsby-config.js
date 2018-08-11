a00999ca2e2bd57b91fa2c29c6c91e6f1763773c
MatthewKosloski <matthewkosloski@me.com>
2018-08-11 02:46:28 -0500

    added more plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,37 @@
-module.exports = {
+
+const isDev = () => process.env.NODE_ENV === 'development';
+
+let pathPrefix = isDev() ? '' : '/labs/pixem';
+
+const common = {
     plugins: [
         'gatsby-plugin-typescript',
-        'gatsby-plugin-styled-components'
+        'gatsby-plugin-styled-components',
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-117972211-1",
+                head: false
+            }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+                options: {
+                    name: "Pixem",
+                    short_name: "Pixem",
+                    start_url: `${pathPrefix}/editor`,
+                    background_color: "#212329",
+                    theme_color: "#4CACCD",
+                    display: "standalone",
+                    icon: "static/pixem.png",
+                }
+        }
     ]
-}
+};
+
+const config = isDev() 
+    ? Object.assign({}, common) 
+    : Object.assign({}, common, {pathPrefix});
+
+module.exports = config;
